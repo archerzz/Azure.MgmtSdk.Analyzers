ddb59ed5ffe7f70aab773d77fd9b0558c9d5cd4d
Mingzhe Huang <archerzz@gmail.com>
2022-08-26 10:53:13 +0800

    update test cases
    - rename test methods
    - add more test cases

--- Azure.MgmtSdk.Analyzers.Test/IntervalPropertyNameAnalyzerTests.cs
+++ Azure.MgmtSdk.Analyzers.Test/IntervalPropertyNameAnalyzerTests.cs
@@ -9,9 +9,12 @@ namespace Azure.MgmtSdk.Analyzers.Test
     public class IntervalPropertyNameAnalyzerTests
     {
         [TestMethod]
-        public async Task AZM0020PublicBoolTypeWithVerbPrefix()
+        public async Task ValidCases()
         {
-            var test = @"namespace Azure.ResourceManager.Network
+            var test = @"using System.Collections.Generic;
+using System.Collections;
+
+namespace Azure.ResourceManager.Network
 {
     public partial class Test
     {
@@ -19,6 +22,9 @@ namespace Azure.MgmtSdk.Analyzers.Test
         public int TestDurationInSeconds { get; set; }
         public int Foo { get; set; }
         public bool TestInterval { get; set; }
+        public string StringInterval { get; set; }
+        public double DoubleInterval { get; set; }
+        public IList<string> IListInterval { get; set; }
         public int IntervalInSeconds { get; set; }
         public int IntervalInMinutes { get; set; }
     }
@@ -27,7 +33,7 @@ namespace Azure.MgmtSdk.Analyzers.Test
         }
 
         [TestMethod]
-        public async Task AZM0020PublicBoolTypeWithoutVerbPrefix()
+        public async Task ErrorCases()
         {
             var test = @"namespace Azure.ResourceManager.Network
 {
@@ -35,15 +41,21 @@ namespace Azure.MgmtSdk.Analyzers.Test
     {
         public int FipInterval { get; set; }
         public int FipDuration { get; set; }
-        public int Interval { get; set; }
-        public int Duration { get; set; }
+        public uint Interval { get; set; }
+        public short Duration { get; set; }
+        public ushort Uint16Interval { get; set; }
+        public long Int64Interval { get; set; }
+        public ulong Uint64Duration { get; set; }
     }
 }";
             await VerifyCS.VerifyAnalyzerAsync(test,
                 VerifyCS.Diagnostic(IntervalPropertyNameAnalyzer.DiagnosticId).WithSpan(5, 20, 5, 31).WithArguments("FipInterval"),
                 VerifyCS.Diagnostic(IntervalPropertyNameAnalyzer.DiagnosticId).WithSpan(6, 20, 6, 31).WithArguments("FipDuration"),
-                VerifyCS.Diagnostic(IntervalPropertyNameAnalyzer.DiagnosticId).WithSpan(7, 20, 7, 28).WithArguments("Interval"),
-                VerifyCS.Diagnostic(IntervalPropertyNameAnalyzer.DiagnosticId).WithSpan(8, 20, 8, 28).WithArguments("Duration")
+                VerifyCS.Diagnostic(IntervalPropertyNameAnalyzer.DiagnosticId).WithSpan(7, 21, 7, 29).WithArguments("Interval"),
+                VerifyCS.Diagnostic(IntervalPropertyNameAnalyzer.DiagnosticId).WithSpan(8, 22, 8, 30).WithArguments("Duration"),
+                VerifyCS.Diagnostic(IntervalPropertyNameAnalyzer.DiagnosticId).WithSpan(9, 23, 9, 37).WithArguments("Uint16Interval"),
+                VerifyCS.Diagnostic(IntervalPropertyNameAnalyzer.DiagnosticId).WithSpan(10, 21, 10, 34).WithArguments("Int64Interval"),
+                VerifyCS.Diagnostic(IntervalPropertyNameAnalyzer.DiagnosticId).WithSpan(11, 22, 11, 36).WithArguments("Uint64Duration")
             );
         }
     }
